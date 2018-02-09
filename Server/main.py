6968b2c8cc7df6f3ca55796e8e51c94ae20731e7
Jennifer <jstark30@gatech.edu>
2018-02-08 19:34:26 -0500

    Changed google maps directions request to mapbox

--- Server/main.py
+++ Server/main.py
@@ -2,7 +2,7 @@ import requests
 from flask import Flask, request
 app = Flask(__name__)
 
-key = "AIzaSyCnMawlWDbstS9T6cVN5GTcF1GuokHUVcM"
+key = "***REMOVED***"
 
 @app.route('/')
 def get_homepage():
@@ -12,7 +12,8 @@ def get_homepage():
 def get_directions():
     origin = request.args.get('origin')
     destination = request.args.get('destination')
-    url = "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + destination + "&mode=walking" + "&key=" + key
+    mode = request.args.get('mode')
+    url = "https://api.mapbox.com/directions/v5/mapbox/" + mode + "/" + origin + ";" + destination + "?access_token=" + key
 
     response = requests.get(url).content
     return response

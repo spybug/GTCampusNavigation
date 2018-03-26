7eb7a0e8f4158d98b94eb726246245dab346819c
Alexander Hennie-Roed <alexhennieroed@gmail.com>
2018-03-26 13:19:31 -0400

    Began working on ScheduleFragment

--- Android/app/src/main/java/com/spybug/gtnav/fragments/ScheduleFragment.java
+++ Android/app/src/main/java/com/spybug/gtnav/fragments/ScheduleFragment.java
@@ -3,12 +3,21 @@ package com.spybug.gtnav.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.spybug.gtnav.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -30,6 +39,9 @@ public class ScheduleFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    private ListView scheduleView;
+    private List<GTNavEvent> eventList;
 
     public ScheduleFragment() {
         // Required empty public constructor
@@ -54,19 +66,43 @@ public class ScheduleFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        View v = inflater.inflate(R.layout.fragment_schedule, container, false);
+
+        eventList = new ArrayList<>();
+        scheduleView = v.findViewById(R.id.ScheduleListView);
+        FloatingActionButton addFab = v.findViewById(R.id.AddToScheduleFAB);
+
+        ArrayAdapter<GTNavEvent> listAdapter =
+                new ArrayAdapter<>(v.getContext(), android.R.layout.simple_list_item_1, eventList);
+        scheduleView.setAdapter(listAdapter);
+
+        scheduleView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //TODO
+                Toast.makeText(ScheduleFragment.this.getContext(), "Event Selected", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        addFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                eventList.add(addEvent());
+                Toast.makeText(v.getContext(), "Event Added", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return v;
+    }
+
+    private GTNavEvent addEvent() {
+        ArrayAdapter<GTNavEvent> listAdapter =
+                new ArrayAdapter<>(ScheduleFragment.this.getContext(),
+                        android.R.layout.simple_list_item_1, eventList);
+        scheduleView.setAdapter(listAdapter);
+        return null;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -100,5 +136,35 @@ public class ScheduleFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+}
+
+class GTNavEvent {
+    private String name;
+    private String[] recurringDays;
+    private String time;
+    private LatLng location;
+
+    GTNavEvent(String name, String time, LatLng location, String[] recurringDays) {
+        this.name = name;
+        this.time = time;
+        this.location = location;
+        this.recurringDays = recurringDays;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String[] getRecurringDays() {
+        return recurringDays;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public LatLng getLocation() {
+        return location;
     }
 }
